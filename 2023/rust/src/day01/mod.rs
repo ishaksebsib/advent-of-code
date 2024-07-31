549c4af4d8553a316fe994b02fb135062f208686
ishak <ishaksebsib@gmail.com>
2024-07-31 10:15:06 +0300

    [ENH]: Add day one solution

---
+++ 2023/rust/src/day01/mod.rs
@@ -0,0 +1,28 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn run() -> u32 {
+    let input = File::open("src/day01/input1.txt").unwrap();
+
+    let reader = BufReader::new(input);
+
+    let output = reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let mut only_digit = line.chars().filter_map(|character| character.to_digit(10));
+            let first_digit = only_digit.next().expect("first digit");
+
+            match only_digit.last() {
+                Some(last_digit) => format!("{first_digit}{last_digit}"),
+                None => format!("{first_digit}{first_digit}"),
+            }
+            .parse::<u32>()
+            .expect("string should be int")
+        })
+        .sum::<u32>();
+
+    output
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,3 +1,5 @@
+mod day01;
 fn main() {
+    day01::run();
     println!("Hello, world!");
 }
