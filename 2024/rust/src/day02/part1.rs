cf45c6146e6ac97a315dd9656f223f054150ee6b
ishaksebsib <ishaksebsib@gmail.com>
2024-12-02 18:37:50 +0300

    [ENH][2024][rust] day2 part1

--- 2024/rust/src/day02/part1.rs
+++ 2024/rust/src/day02/part1.rs
@@ -1,45 +1,58 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
-pub fn run() -> u32 {
-    let file = File::open("src/day02/input1.txt").expect("can't open file");
+pub fn run() -> io::Result<u32> {
+    let file = File::open("src/day02/input1.txt")?;
     let reader = BufReader::new(file);
     let mut output = 0;
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let line_vec: Vec<i32> = line
+        let line = line?;
+        let line_vec: Result<Vec<i32>, _> = line
             .split_whitespace()
-            .map(|x| x.parse::<i32>().unwrap())
+            .map(str::parse)
             .collect();
 
-        let mut is_increasing = true;
-        let mut is_decreasing = true;
-        let mut pass = true;
-
-        for i in 0..line_vec.len() - 1 {
-            let diff = line_vec[i + 1] - line_vec[i];
-
-            if diff.abs() < 1 || diff.abs() > 3 {
-                pass = false;
-                break;
+        let line_vec = match line_vec {
+            Ok(vec) => vec,
+            Err(_) => {
+                eprintln!("Skipping invalid line: {}", line);
+                continue;
             }
+        };
 
-            if diff > 0 {
-                is_decreasing = false;
-            } else if diff < 0 {
-                is_increasing = false;
-            }
-        }
-
-        if !(is_increasing || is_decreasing) {
-            pass = false;
-        }
-
-        if pass {
+        if is_valid_sequence(&line_vec) {
             output += 1;
         }
     }
 
     println!("{}", output);
-    output
+    Ok(output)
+}
+
+fn is_valid_sequence(line_vec: &[i32]) -> bool {
+    if line_vec.len() < 2 {
+        return false;
+    }
+
+    let mut is_increasing = true;
+    let mut is_decreasing = true;
+
+    for window in line_vec.windows(2) {
+        let diff = window[1] - window[0];
+
+        if !(1..=3).contains(&diff.abs()) {
+            return false;
+        }
+
+        if diff > 0 {
+            is_decreasing = false;
+        } else if diff < 0 {
+            is_increasing = false;
+        }
+    }
+
+    is_increasing || is_decreasing
 }
